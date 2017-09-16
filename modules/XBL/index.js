a8b6ebe908d4b1d59613d4b634247009845f4fdc
Brandt Abbott <babbott@layer3tv.com>
2017-09-16 09:29:49 -0600

    Changed selector to look for friendsContent ID.  Added default for not status info

--- modules/XBL/index.js
+++ modules/XBL/index.js
@@ -3,34 +3,34 @@ var XBOX_LOGIN = process.env['XBOX_LOGIN'];
 var XBOX_PASSWORD = process.env['XBOX_PASSWORD'];
 var Nightmare = require('nightmare');
 var cheerio = require("cheerio");
-var gamerListSelector = '[class=gamerList]';
+var gamerListSelector = '[id=friendsContent]';
 
 xbl = {
   name: 'xbl',
   helpText: '/xbl [gamertag] - Shows XBL info for gamertag\n*  /xbl status - Shows members online',
-  defaultFunction: function(args, bot) {return xbl.do(args, bot)}   
+  defaultFunction: function(args, bot) {return xbl.do(args, bot)}
 };
 
 xbl.do = function(args, bot){
 };
 
-xbl.status = function(args, bot){  
-  var group_members = GROUP_GAMERTAGS.split(',');  
+xbl.status = function(args, bot){
+  var group_members = GROUP_GAMERTAGS.split(',');
   var group_playing = '';
-  var nightmare = Nightmare({ 
+  var nightmare = Nightmare({
     show: true,
-    switches: {      
+    switches: {
       'ignore-certificate-errors': true
-    } 
+    }
   });
-  
+
   nightmare
     .goto('https://login.live.com')
     .insert('input[name=loginfmt]', XBOX_LOGIN)
-    .click('input[value="Next"]')  
+    .click('input[value="Next"]')
     .wait(5000)
     .insert('input[name=passwd]', XBOX_PASSWORD)
-    .click('input[value="Sign in"]')  
+    .click('input[value="Sign in"]')
     .wait(2000)
     .goto('https://account.xbox.com/en-US/Friends')
     .wait(gamerListSelector)
@@ -44,15 +44,15 @@ xbl.status = function(args, bot){
       $("ul > li").each(function(){
         var li = $(this);
         var gamerTag = li.attr("data-gamertag");
-        var info = li.find(".primaryInfo").text();
+        var info = li.find(".primaryInfo").text() || 'Home';
         if(!info.startsWith('Offline') && !info.startsWith('Last seen') && group_members.indexOf(gamerTag) != -1)
           group_playing = group_playing + gamerTag+': '+info+'\n';
       });
       bot.message(group_playing);
-    })  
+    })
     .catch(function (error) {
       console.error('Search failed:', error);
-    });  
+    });
 };
 
 module.exports = xbl;
